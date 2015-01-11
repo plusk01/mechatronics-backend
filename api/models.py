da76c81059e910b959f0f1f826ad15c6a81ca0cd
Parker Lusk <parkerclusk@gmail.com>
2015-01-11 12:47:13 -0700

    mailchimp integration

--- api/models.py
+++ api/models.py
@@ -7,12 +7,29 @@ from django.dispatch import receiver
 from django.utils import timezone
 from rest_framework.authtoken.models import Token
 
+import mailchimp
+
 from api.managers import MemberObjectsManager, MemberManager
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def subscribe_member(sender, instance=None, created=False, **kwargs):
+	if created:
+		try:
+			m = mailchimp.Mailchimp(settings.MAILCHIMP_APIKEY)
+			m.lists.subscribe(settings.MAILCHIMP_LISTID, {
+					'email': instance.email,
+				},
+				merge_vars={
+					'FNAME': instance.first_name,
+					'LNAME': instance.last_name,
+				}, double_optin=False)
+		except:
+			pass
 
 # Create your models here.
 class FieldOfStudy(models.Model):
