dede68807a19aa48a661647e6536b0e3af5d8282
Parker Lusk <parkerclusk@gmail.com>
2015-07-17 10:35:26 -0600

    changing MEDIA_URL

--- mechatronics/settings.py
+++ mechatronics/settings.py
@@ -100,7 +100,7 @@ STATIC_ROOT = '/srv/www/vhosts/byumechatronics/static/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://byumechatronics.com/media/'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
